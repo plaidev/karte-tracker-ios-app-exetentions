48861eeed37027b800371f84037bd9db4f20a1b9
plaid-inc <dev.share@plaid.co.jp>
2018-08-06 03:22:35 -0700

    1.0.1

--- KarteTrackerAppExtensions.framework/Headers/KarteNotificationServiceExtension.h
+++ KarteTrackerAppExtensions.framework/Headers/KarteNotificationServiceExtension.h
@@ -9,6 +9,9 @@
 #import <UserNotifications/UserNotifications.h>
 
 __IOS_AVAILABLE(10.0) __TVOS_PROHIBITED __WATCHOS_PROHIBITED
+
 @interface KarteNotificationServiceExtension : UNNotificationServiceExtension
+
+- (BOOL)canHandleRemoteNotification:(nonnull NSDictionary *)userInfo;
 
 @end
